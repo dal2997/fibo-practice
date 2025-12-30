7cbc4a0b3867dc468c5dcc5cbf9374ba177f3690
dal2997 <ys2997@naver.com>
2025-12-30 14:25:35 +0900

    feat: make memorization.py

---
+++ memorization.py
@@ -0,0 +1,9 @@
+def fibo(n): 
+    pad = {0: 0, 1: 1} 
+    def fib_inner(n): 
+        if n not in pad: 
+            pad[n] = fib_inner(n - 1) + fib_inner(n - 2) 
+        return pad[n] 
+    return fib_inner(n) 
+if __name__ == "__main__": 
+    print(fibo(10))
